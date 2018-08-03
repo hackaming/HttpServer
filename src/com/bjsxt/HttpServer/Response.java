0bb1bc17c3271c7c89bdd9ceb9fabd2eb84190da
Yan <Xianming.Yan@us.intercall.com>
2018-08-03 15:06:43 +0800

    Signed-off-by: Yan <Xianming.Yan@us.intercall.com> just debugged to see if the response can be print out.

--- src/com/bjsxt/HttpServer/Response.java
+++ src/com/bjsxt/HttpServer/Response.java
@@ -1,5 +1,21 @@
 package com.bjsxt.HttpServer;
-
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public class Response {
-
+	BufferedWriter br;
+	StringBuilder sb ;
+	public Response(){
+		br= null;
+		sb = new StringBuilder();
+		sb.append("<html><head><meta charset=\"UTF-8\"><title>ResponseTestWithChinesecharacter</title></head><body>Hello World!	ÄãºÃ£¬ÊÀ½ç£¡	</body>	</html>");
+	}
+	public Response(OutputStream os){
+		this();
+		br = new BufferedWriter(new OutputStreamWriter(os));
+	}
+/*	public static void main(String[] argv){
+		Response r = new Response();
+		System.out.println(r.sb);
+	}*/
 }
