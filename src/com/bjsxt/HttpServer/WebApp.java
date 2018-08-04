55831a206b4a070f092d7010988cfcc508be4ba7
hackaming <2251275876@qq.com>
2018-08-04 21:10:59 +0800

    Tried the SAXParser  to analyze a xml file. There are total 4 steps like below: //1、获取解析工厂 		SAXParserFactory factory=SAXParserFactory.newInstance(); 		//2、从解析工厂获取解析器 		SAXParser parse =factory.newSAXParser(); 		//3、加载文档 Document 注册处理器 		//4、编写处理器 		PersonHandler handler=new PersonHandler(); 		parse.parse(Thread.currentThread().getContextClassLoader() 				.getResourceAsStream("com/bjsxt/xml/person.xml") 				,handler );

--- src/com/bjsxt/HttpServer/WebApp.java
+++ src/com/bjsxt/HttpServer/WebApp.java
@@ -1,30 +1,34 @@
 package com.bjsxt.HttpServer;
+
 import java.util.*;
+
 public class WebApp {
     private static ServletContext servletContext;
-    static{ //read all the message from the web.xml, here just simulate it by adding it manually
+
+    static { //read all the message from the web.xml, here just simulate it by adding it manually
         servletContext = new ServletContext();
-        Map<String,String> mapping = servletContext.getMapping();
-        Map<String,String> servlets = servletContext.getServlets();
+        Map<String, String> mapping = servletContext.getMapping();
+        Map<String, String> servlets = servletContext.getServlets();
         servlets.put("login", "com.bjsxt.HttpServer.LoginServlet");
         servlets.put("register", "com.bjsxt.HttpServer.RegisterServlet");
-        mapping.put("/login","login");
-        mapping.put("/register","register");
+        mapping.put("/login", "login");
+        mapping.put("/register", "register");
+    }
+
+    public static Servlet getServlet(String url) {
+        String s = servletContext.getMapping().get(url);
+        String ser = servletContext.getServlets().get(s);
+        try {
+            return (Servlet) Class.forName(ser).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        public static Servlet getServlet(String url){
-            String s = servletContext.getMapping().get(url);
-            String  ser = servletContext.getServlets().get(s);
-            try {
-                return (Servlet)Class.forName(ser).newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
+        return null;
+    }
 /*        public static void main(String argv[]){
             WebApp w = new WebApp();
             String s = w.servletContext.getMapping().get("/register");
