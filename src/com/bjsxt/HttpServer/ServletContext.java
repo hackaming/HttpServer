cafd7c125aac455ad867688d4f5184d6e6f57f02
hackaming <2251275876@qq.com>
2018-08-04 16:19:37 +0800

    Change the Serverlet to string and get it from reflect.

--- src/com/bjsxt/HttpServer/ServletContext.java
+++ src/com/bjsxt/HttpServer/ServletContext.java
@@ -1,18 +1,18 @@
 package com.bjsxt.HttpServer;
 import java.util.*;
 public class ServletContext { //a container includes all the servlet information
-    Map<String,Servlet> servlets; //url to servlet
+    Map<String,String> servlets; //url to servlet
     Map<String,String> mapping; //string to url?
     public ServletContext(){
-        servlets = new HashMap<String,Servlet>();
+        servlets = new HashMap<String,String>();
         mapping = new HashMap<String,String>();
     }
 
-    public Map<String, Servlet> getServlets() {
+    public Map<String, String> getServlets() {
         return servlets;
     }
 
-    public void setServlets(Map<String, Servlet> servlets) {
+    public void setServlets(Map<String, String> servlets) {
         this.servlets = servlets;
     }
     public Map<String, String> getMapping() {
--- src/com/bjsxt/HttpServer/WebApp.java
+++ src/com/bjsxt/HttpServer/WebApp.java
@@ -5,16 +5,25 @@ public class WebApp {
     static{ //read all the message from the web.xml, here just simulate it by adding it manually
         servletContext = new ServletContext();
         Map<String,String> mapping = servletContext.getMapping();
-        Map<String,Servlet> servlets = servletContext.getServlets();
-        servlets.put("login", new LoginServlet());
-        servlets.put("register", new RegisterServlet());
+        Map<String,String> servlets = servletContext.getServlets();
+        servlets.put("login", "com.bjsxt.HttpServer.LoginServlet");
+        servlets.put("register", "com.bjsxt.HttpServer.RegisterServlet");
         mapping.put("/login","login");
         mapping.put("/register","register");
         }
         public static Servlet getServlet(String url){
             String s = servletContext.getMapping().get(url);
-            Servlet ser = servletContext.getServlets().get(s);
-            return ser;
+            String  ser = servletContext.getServlets().get(s);
+            try {
+                return (Servlet)Class.forName(ser).newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
 /*        public static void main(String argv[]){
             WebApp w = new WebApp();
