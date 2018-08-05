4b65e5b5392ac360c48cebe3633f0cdfff8c5504
hackaming <2251275876@qq.com>
2018-08-05 08:38:05 +0800

    基本上完成了，整个功能方面。 下一步就是要将关键的类抽出来。放到一个包里面，这些是不用动的。 实际应用的时候，只需要从SERVLET继承，并写实际的业务。并将这些业务配置到WEBAPP.XML之中，就可以了。

--- src/com/bjsxt/HttpServer/Servlet.java
+++ src/com/bjsxt/HttpServer/Servlet.java
@@ -1,10 +1,10 @@
 package com.bjsxt.HttpServer;
 
 public abstract class Servlet {
-    public void service(Request req,Response resp){
+    protected void service (Request req,Response resp){
         this.doGet( req, resp);
         this.doPost( req, resp);
     }
-    public abstract void doGet(Request req,Response resp);
-    public abstract void doPost(Request req,Response resp);
+    protected abstract void doGet(Request req,Response resp);
+    protected abstract void doPost(Request req,Response resp);
 }
